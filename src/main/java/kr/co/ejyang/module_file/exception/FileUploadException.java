1a30430400676db29b2c328fd412901eeda6c064
ejyang <trustnloveu@gmail.com>
2023-05-23 17:02:13 +0900

    FileUploadException 기본 생성자 주석(import 에러 원인)

--- src/main/java/kr/co/ejyang/module_file/exception/FileUploadException.java
+++ src/main/java/kr/co/ejyang/module_file/exception/FileUploadException.java
@@ -3,7 +3,7 @@ package kr.co.ejyang.module_file.exception;
 public class FileUploadException extends RuntimeException {
 
     // 생성자 1
-    FileUploadException() { }
+//    FileUploadException() { }
 
     // 생성자 2
     public FileUploadException(String message) {
